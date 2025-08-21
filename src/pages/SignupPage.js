65cf8ffba1cde425cfb331eb4dfd55086f1f0f85
Sanghyeop <xoxoxmin1268@naver.com>
2025-08-21 13:52:11 +0900

    이메일 및 닉네임 중복로직 API 추가

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -23,22 +23,49 @@ const SignupPage = () => {
         }
 
         try{
-            //2.백엔드에 회원가입 API 호출
+            //2.닉네임 중복 확인 API 호출
+            const nicknameRes = await axios.get(`http://localhost:8080/api/auth/check-nickname?nickname=${nickname}`);
+            if(nicknameRes.data.isDuplicate) {
+                setError('이미 사용 중인 닉네임입니다.');
+                alert('이미 사용 중인 닉네임입니다.');
+
+                return;
+            }
+
+            //3.이메일 중복 확인 API 호출
+            const emailRes = await axios.get(`http://localhost:8080/api/auth/check-email?email=${email}`);
+            if(emailRes.data.isDuplicate) {
+                setError('이미 가입된 이메일입니다.');
+                alert('이미 가입된 이메일입니다.');
+
+                return;
+            }
+
+
+            //4.백엔드에 회원가입 API 호출
             await axios.post('http://localhost:8080/api/auth/signup', {
                 email,
                 password,
                 nickname
             });
 
-            //3.성공 시 로그인 페이지로 이동
+            //5.성공 시 로그인 페이지로 이동
             alert('회원가입에 성공했습니다! 로그인 페이지로 이동합니다.');
             navigate('/login');
         } catch(err) {
             //4.실패 시 에러 처리
-            if(err.response && err.response.data) {
-                setError(err.response.data.message || '회원가입에 실패했습니다.');
+            if(err.response && err.response.data && typeof err.response.data === 'string') {
+                //백엔드 응답이 문자열이라면 그 값을 사용(백엔드가 예외 메시지를 직접 반환하는 경우)
+                const errorMessage = err.response.data;
+                setError(errorMessage);
+                alert(errorMessage);
+            } else if(err.response && err.response.data && err.response.data.message) {
+                const errorMessage = err.response.data.message;
+                setError(errorMessage);
+                alert(errorMessage);
             } else {
-                setError('서버에 연결할 수 없습니다.');
+                setError('회원가입 처리 중 오류가 발생했습니다.');
+                alert('회원가입 처리 중 오류가 발생했습니다.')
             }
             console.error('Signup failed:', err);
         }
